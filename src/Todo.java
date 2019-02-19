f3c9027f46bd8326a9803d4dde1a8eb5bc73965f
Aaron Craig <aaron.t.craig-1@ou.edu>
2019-02-18 21:11:43 -0600

    Finished the todo.java

--- src/Todo.java
+++ src/Todo.java
@@ -1,18 +1,33 @@
 public class Todo {/*
-Lab Section_________: XX
-Last Modified_______: 20XX-XX-XX
-OU ID_______________: XXXXXXXXX
-OU EMAIL____________: XXX@ou.edu
+Lab Section_________: 011
+Last Modified_______: 2019-02-12
+OU ID_______________: 113365209
+OU EMAIL____________: aaron.t.craig-1@ou.edu
 
 Tasks:
-1. XXX
-2. XXX
+1. complete the calculateTwoTokens method in calculator.java
+2. complete the calculateThreeTokens method in calculator.java
+3. complete the execute method in calculator.java
+4. complete the parseAndExecute method in calculator.java
+5. Run and debug calculator.java and make sure it works properly
+6. Write all the calculatorTest methods in calculatorTest.java
+7. Run and debug the testRunner and make sure all tests work properly
 
 Estimated Time (minutes):
-1. XXX
-2. XXx
+1. 5
+2. 10
+3. 10
+4. 10
+5. 5
+6. 20
+7. 5
 
 Actual Time (minutes):
-1. XXX
-2. XXX
+1. 5
+2. 5
+3. 15
+4. 20
+5. 15
+6. 30
+7. 5
 */}
